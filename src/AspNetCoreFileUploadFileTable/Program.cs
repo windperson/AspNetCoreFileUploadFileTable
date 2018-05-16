5ff3019338873694acb7d69903b54b4a99e720e0
Chen Yu Pao <windperson@gmail.com>
2018-05-16 14:54:09 +0800

    prepare for db migrate

--- src/AspNetCoreFileUploadFileTable/Program.cs
+++ src/AspNetCoreFileUploadFileTable/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
 namespace AspNet5FileUploadFileTable
@@ -11,14 +12,18 @@ namespace AspNet5FileUploadFileTable
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
+            var host = BuildWebHost(args);
+
+            host.Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)=>
+            WebHost.CreateDefaultBuilder(args)
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
 
-            host.Run();
-        }
     }
 }
