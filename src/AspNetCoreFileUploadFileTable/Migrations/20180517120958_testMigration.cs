7f62701c9dc3e63d1f1680ecd3af73e28f255171
Chen Yu Pao <windperson@gmail.com>
2018-05-17 20:11:55 +0800

    add 1st db migration

---
+++ src/AspNetCoreFileUploadFileTable/Migrations/20180517120958_testMigration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace AspNetCoreFileUploadFileTable.Migrations
+{
+    public partial class testMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FileDescriptions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ContentType = table.Column<string>(nullable: true),
+                    CreatedTimestamp = table.Column<DateTime>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    FileName = table.Column<string>(nullable: true),
+                    UpdatedTimestamp = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FileDescriptions", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FileDescriptions");
+        }
+    }
+}
